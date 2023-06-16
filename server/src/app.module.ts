9b4663c889933225bb30ba8e48c07268dd9df87d
Tuan Nguyen <tuan.nguyen@pops.vn>
2023-06-17 00:22:48 +0700

    create server source code structure

---
+++ server/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { VideosModule } from './modules/videos/videos.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, VideosModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
